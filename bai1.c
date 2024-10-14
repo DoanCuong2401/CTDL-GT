bef977a2e1ec59eae78fe97220ca1927231ca3da
DoanCuong2401 <doancuong2410@gmail.com>
2024-10-14 09:59:04 +0700

    loi

--- bai1.c
+++ bai1.c
@@ -10,7 +10,7 @@ int main() {
     for(int i = 0; i < 9; i++) {
         for(int j = i + 1; j < 9; j++) {
             if(arr[i] > arr[j]){
-                swap(&arr[i], &arr[j]);
+                swap(&arr[i], &arr[j])
             }
         }
     }
